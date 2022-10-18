50ffb3635a5e57f47ce3945a30ec164c1e4a155c
ayamir <lgt986452565@gmail.com>
2022-10-18 20:16:41 +0800

    fix: cors settings.

--- src/server.ts
+++ src/server.ts
@@ -9,50 +9,50 @@ import Options from "./class/options";
 import { reset as resetHandler } from "./class/httphandler";
 
 export const createServer = (config: Options): express.Application => {
-  const cors = require("cors");
-  const corsOptions: cors.CorsOptions = {
-    allowedHeaders: [
-      "Origin",
-      "X-Requested-With",
-      "Content-Type",
-      "Accept",
-      "X-Access-Token"
-    ],
-    methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
-    origin: ["https://bupt.wanl5.top:5000"]
-  };
-  const app: express.Application = express();
-  resetHandler(config.mode);
-  // logging http access
-  if (config.logging != "none") {
-    app.use(morgan(config.logging));
-  }
-  // const signal = require('./signaling');
-  app.use(cors(corsOptions));
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
-  app.get("/config", (req, res) =>
-    res.json({
-      useWebSocket: config.websocket,
-      startupMode: config.mode,
-      logging: config.logging
-    })
-  );
-  app.use("/signaling", signaling);
-  app.use(express.static(path.join(__dirname, "../client/public")));
-  app.get("/", (req, res) => {
-    const indexPagePath: string = path.join(
-      __dirname,
-      "../client/public/index.html"
+    const cors = require("cors");
+    const corsOptions: cors.CorsOptions = {
+        allowedHeaders: [
+            "Origin",
+            "X-Requested-With",
+            "Content-Type",
+            "Accept",
+            "X-Access-Token"
+        ],
+        methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+        origin: ["https://10.112.79.143:5000", "https://10.112.102.131:5000"]
+    };
+    const app: express.Application = express();
+    resetHandler(config.mode);
+    // logging http access
+    if (config.logging != "none") {
+        app.use(morgan(config.logging));
+    }
+    // const signal = require('./signaling');
+    app.use(cors(corsOptions));
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+    app.get("/config", (req, res) =>
+        res.json({
+            useWebSocket: config.websocket,
+            startupMode: config.mode,
+            logging: config.logging
+        })
     );
-    fs.access(indexPagePath, (err) => {
-      if (err) {
-        log(LogLevel.warn, `Can't find file ' ${indexPagePath}`);
-        res.status(404).send(`Can't find file ${indexPagePath}`);
-      } else {
-        res.sendFile(indexPagePath);
-      }
+    app.use("/signaling", signaling);
+    app.use(express.static(path.join(__dirname, "../client/public")));
+    app.get("/", (req, res) => {
+        const indexPagePath: string = path.join(
+            __dirname,
+            "../client/public/index.html"
+        );
+        fs.access(indexPagePath, (err) => {
+            if (err) {
+                log(LogLevel.warn, `Can't find file ' ${indexPagePath}`);
+                res.status(404).send(`Can't find file ${indexPagePath}`);
+            } else {
+                res.sendFile(indexPagePath);
+            }
+        });
     });
-  });
-  return app;
+    return app;
 };
